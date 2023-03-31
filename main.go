c0e5f76dd6474c22653ef42faf5deff0fcb674e1
lalizita <laislima98@hotmail.com>
2023-03-31 10:55:19 -0300

    Improve watcher function

--- main.go
+++ main.go
@@ -9,44 +9,36 @@ import (
 )
 
 func main() {
-	log.Println("Create watcher")
+	fileUpdated := make(chan string)
+
+	go WatchEvents(fileUpdated)
+
+	for f := range fileUpdated {
+		fmt.Println("File updated:", f)
+	}
+}
+
+func WatchEvents(updateF chan<- string) {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer watcher.Close()
 
-	// Start listening for events.
-	go func() {
-		for {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					return
-				}
-				log.Println("event:", event)
-				if event.Has(fsnotify.Write) {
-					log.Println("modified file:", event.Name)
-				}
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					return
-				}
-				log.Println("error:", err)
-			}
-		}
-	}()
-
-	// Add a path.
 	path, _ := os.Getwd()
-	file_path := fmt.Sprintf("%s/text", path)
-	log.Printf("Watching: %s", file_path)
-	err = watcher.Add(file_path)
+	filep := fmt.Sprintf("%s/text/text.txt", path)
+	err = watcher.Add(filep)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Block main goroutine forever.
-	<-make(chan struct{})
+	if err != nil {
+		log.Fatal("Error in file path:", err.Error())
+	}
+
+	for event := range watcher.Events {
+		if event.Has(fsnotify.Write) {
+			updateF <- event.Name
+		}
+	}
 }
